c5ed7468c5834c2605ed315420bc22ae9794fa74
406-00\teacher <test>
2024-10-01 12:20:55 +0900

    Rayクラスを使用してポイントへ投げるように変更

---
+++ Assets/Script/IgaguriGenerator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriGenerator : MonoBehaviour
+{
+    public GameObject igaguriPrefab;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject igaguri = Instantiate(igaguriPrefab);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 wolrdDir = ray.direction;
+            igaguri.GetComponent<IgaguriController>().Shoot(wolrdDir.normalized * 2000);
+        }
+    }
+}
